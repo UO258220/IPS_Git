e28d3b2bdfd3d84fcd6d04878fd8b23cac21d922
UO258220 <UO258220@EII-LIBRE-06.mshome.net>
2022-10-14 12:02:07 +0200

    Nuevos cambios en master

--- IPS_Git_2122/src/Main.java
+++ IPS_Git_2122/src/Main.java
@@ -3,19 +3,17 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("---");
+		
 		System.out.println("Hello world!");
 		
-		System.out.println();
+		System.out.println("---");
 		
-		System.out.println("Añadido algo");
+		System.out.println("Añadido todo");
 		
-		System.out.println();
+		System.out.println("---");
 		
 		System.out.println("Añadido algo más");
-		
-		System.out.println();
-		
-		System.out.println("Un extra");
 		
 		// Solamente un comentario la verdad
 		// Todo correcto otra vez
